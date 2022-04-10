9bb550da072b2089837e527fc0939ceb784e5063
Mahdi Mattout <mahdim@lemonadefashion.com>
2022-04-10 23:59:20 +0300

    added all packages

--- index.js
+++ index.js
@@ -1,44 +1,51 @@
-const { MongoClient } = require("mongodb");
+const { MongoClient, ObjectId } = require("mongodb");
 const connectionString = 'mongodb+srv://TodoAppMahdi:TodoAppAsst123@todoapp.kym6d.mongodb.net/TodoApp?retryWrites=true&w=majority'
 const express = require('express');
-const fs = require('fs');
 const app = express();
+app.set('view engine', 'ejs')
 
-const whenFirstLoaded = (request, response) => {
-  response.writeHead(200, {
-    'Content-Type': 'text/html'
+
+const getStatus = (task) => {
+  return task.isComplete ? 'Complete' : 'In Progress';
+}
+
+const whenFirstLoaded = (collection, response) => {
+  return collection.find().toArray().then(tasks => {
+    response.render('index.ejs', {
+      tasks: tasks,
+      getStatus: getStatus
+    });
   });
-  fs.readFile('./index.html', null, (err, data) => {
-    if (err) {
-      response.writeHead(404);
-      response.write('File not found');
-    } else {
-      response.write(data);
-    }
-    response.end();
-  })
 }
 
-const main = async () => {
-  const client = new MongoClient(connectionString);
-  try {
-    await client.connect();
-    // await listDatabases(client);
-  } catch (e) {
-    console.error(e);
-  } finally {
-    await client.close();
-  }
-}
+MongoClient.connect(connectionString)
+  .then(client => {
+    console.log('Connected Successfully!');
+    const db = client.db();
+    const tasksCollection = db.collection('tasks');
+    app.get('/', (req, res) => {
+      return whenFirstLoaded(tasksCollection, res);
+    })
+    app.get('/task', (req, res) => {
+      return whenFirstLoaded(tasksCollection, res)
+    })
+    app.post('/task', (req, res) => {
+      req.body['isComplete'] = false;
+      tasksCollection.insertOne(req.body).then(() => whenFirstLoaded(tasksCollection, res))
+    })
 
-// const listDatabases = async client => {
-//   const list = await client.db().admin().listDatabases();
-//   list.databases.forEach(db => {
-//     console.log(` - ${db.name}`)
-//   })
-// }
-
-main().catch(console.error)
+    app.delete('/task/:id', (req, res) => {
+      const id = ObjectId(req.params.id);
+      return tasksCollection.deleteOne({ _id: id })
+        .then(result => {
+          if (result.deletedCount === 0) {
+            res.send('Task not found')
+          } else {
+            return whenFirstLoaded(tasksCollection, res);
+          }
+        })
+    })
+})
 
 app.listen(8000);
 
